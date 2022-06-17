a1e3f5899b0ebdfdf6d24c7a3ed5436e2eaf7d16
jorge <geolg763@gmail.com>
2022-06-17 16:01:16 -0500

    to create wterfall efect to a image

--- scripts/index.js
+++ scripts/index.js
@@ -9,11 +9,36 @@ image1.src = '../img/FTdTn7zWAAU20lz.jpeg'
 image1.addEventListener('load', () => {
     ctx.drawImage(image1, 0, 0, canvas.width, canvas.heigth/4)
     const pixels = ctx.getImageData(0, 0, canvas.width, canvas.heigth)
+    // ctx.clearRect(0,0, canvas.width, canvas.heigth)
     console.log(pixels)
     let particlesArray = []
-    const numberOfParticles = 5000
+    const numberOfParticles = 2000
 
     let mappedImage = [];
+    for( let y = 0; y < canvas.heigth; y++){
+        let row = []
+        for(let x = 0; x < canvas.width; x++){
+            const red = pixels.data[( y * 4 * pixels.width) + ( x * 4 )]
+            const green = pixels.data[( y * 4 * pixels.width) + ( x * 4 + 1 )]
+            const blue = pixels.data[( y * 4 * pixels.width) + ( x * 4 + 2 )]
+            const brightness = calculateRelativeBrightness(red, green, blue)
+            const cell = [
+                cellbrigthness = brightness,
+            ]
+            row.push(cell)
+        }
+        mappedImage.push(row)
+    }
+
+    console.log(mappedImage)
+
+    function calculateRelativeBrightness(red, green, blue){
+        return Math.sqrt(
+            (red * red) * 0.299 +
+            (green * green) * 0.587 +
+            (blue * blue) * 0.114
+        )/ 100
+    }
 
     class Particle {
         constructor(){
@@ -22,9 +47,15 @@ image1.addEventListener('load', () => {
             this.speed = 0
             this.velocity = Math.random() * 0.5
             this.size = Math.random() * 1.5 + 1
+            this.position1 = Math.floor(this.y)
+            this.position2 = Math.floor(this.x)
         }
         update(){
-            this.y += this.velocity
+            this.position1 = Math.floor(this.y)
+            this.position2 = Math.floor(this.x)
+            this.speed = mappedImage[this.position1][this.position2][0]
+            let movement = (2.5 - this.speed ) + this.velocity
+            this.y += movement
             if(this.y >= canvas.heigth){
                 this.y = 0
                 this.x = Math.random() * canvas.width
@@ -44,12 +75,13 @@ image1.addEventListener('load', () => {
     }
     init()
     function animate(){
-        ctx.drawImage(image1, 0, 0, canvas.width, canvas.heigth/4)
         ctx.globalAlpha = 0.05
         ctx.fillStyle = 'rgb(0,0,0)'
         ctx.fillRect(0,0, canvas.width, canvas.heigth)
+        ctx.globalAlpha = 0.2
         for( let i = 0; i < particlesArray.length; i++){
             particlesArray[i].update()
+            ctx.globalAlpha = particlesArray[i].speed * 0.5
             particlesArray[i].draw()
         }
         requestAnimationFrame(animate)
