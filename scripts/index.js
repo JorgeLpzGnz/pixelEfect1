88e40536857da04586d769856f3b7a05a0e5011d
jorge <geolg763@gmail.com>
2022-06-21 08:12:19 -0500

    to add flow efects, and some filters

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,28 @@
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
-canvas.width = 500;
-canvas.heigth = 706;
+canvas.width = 1024;
+canvas.heigth = 1280;
+
+// gadients
+
+const gradient1 = ctx.createLinearGradient(0, 0, canvas.heigth, canvas.width)
+gradient1.addColorStop(0.2, 'pink')
+gradient1.addColorStop(0.3, 'red')
+gradient1.addColorStop(0.4, 'orange')
+gradient1.addColorStop(0.5, 'yellow')
+gradient1.addColorStop(0.6, 'white')
+gradient1.addColorStop(0.7, 'turquoise')
+gradient1.addColorStop(0.8, 'violet')
 
 const image1 = new Image();
 image1.src = '../img/FTdTn7zWAAU20lz.jpeg'
 
 image1.addEventListener('load', () => {
-    ctx.drawImage(image1, 0, 0, canvas.width, canvas.heigth/4)
+    ctx.drawImage(image1, 0, 0, canvas.width, canvas.heigth/6)
     const pixels = ctx.getImageData(0, 0, canvas.width, canvas.heigth)
-    // ctx.clearRect(0,0, canvas.width, canvas.heigth)
-    console.log(pixels)
+    ctx.clearRect(0,0, canvas.width, canvas.heigth)
     let particlesArray = []
-    const numberOfParticles = 2000
+    const numberOfParticles = 300
 
     let mappedImage = [];
     for( let y = 0; y < canvas.heigth; y++){
@@ -24,6 +34,7 @@ image1.addEventListener('load', () => {
             const brightness = calculateRelativeBrightness(red, green, blue)
             const cell = [
                 cellbrigthness = brightness,
+                cellColor = 'rgb(' + red + ',' + green + ',' + blue + ')'
             ]
             row.push(cell)
         }
@@ -45,26 +56,42 @@ image1.addEventListener('load', () => {
             this.x = Math.random() * canvas.width
             this.y = 0
             this.speed = 0
-            this.velocity = Math.random() * 0.5
-            this.size = Math.random() * 1.5 + 1
+            this.velocity = Math.random() * 0.4
+            this.size = Math.random() * 2.5 + 0.2
             this.position1 = Math.floor(this.y)
             this.position2 = Math.floor(this.x)
+            this.angle = 0
         }
         update(){
             this.position1 = Math.floor(this.y)
             this.position2 = Math.floor(this.x)
-            this.speed = mappedImage[this.position1][this.position2][0]
+            if( mappedImage[this.position1] &&  mappedImage[this.position1][this.position2]){
+                this.speed = mappedImage[this.position1][this.position2][0]
+            }
             let movement = (2.5 - this.speed ) + this.velocity
-            this.y += movement
+            this.angle += this.speed / 20
+            this.size = this.speed * 2
+
+            this.y += movement + Math.sin(this.angle) * 2
+            this.x += movement + Math.cos(this.angle) * 2
             if(this.y >= canvas.heigth){
                 this.y = 0
                 this.x = Math.random() * canvas.width
             }
+            if(this.x >= canvas.width){
+                this.x = 0
+                this.y = Math.random() * canvas.heigth
+            }
         }
         draw(){
             ctx.beginPath();
-            ctx.fillStyle = 'white'
-            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+            if( mappedImage[this.position1] &&  mappedImage[this.position1][this.position2]){
+                ctx.fillStyle = mappedImage[this.position1][this.position2][1]
+            }
+            // ctx.fillStyle = gradient1
+            // ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+            ctx.font = '100px Arial'
+            ctx.fillText('z', this.x, this.y)
             ctx.fill()
         }
     }
@@ -81,7 +108,7 @@ image1.addEventListener('load', () => {
         ctx.globalAlpha = 0.2
         for( let i = 0; i < particlesArray.length; i++){
             particlesArray[i].update()
-            ctx.globalAlpha = particlesArray[i].speed * 0.5
+            ctx.globalAlpha = 2
             particlesArray[i].draw()
         }
         requestAnimationFrame(animate)
